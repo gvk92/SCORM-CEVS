96d6070b9137ae8046c65f7392b918a752b2038e
gvk92 <venkat.kiran114@gmail.com>
2026-03-26 18:38:36 +0530

    Support python3 alongside python and py launchers

---
+++ scripts/install-python-deps.js
@@ -0,0 +1,12 @@
+const { spawnSync } = require('node:child_process');
+const { detectPythonLauncher } = require('./python-launcher');
+
+try {
+  const python = detectPythonLauncher();
+  const args = ['-m', 'pip', 'install', '-r', 'requirements.txt'];
+  const res = spawnSync(python, args, { stdio: 'inherit', shell: process.platform === 'win32' });
+  process.exit(res.status ?? 1);
+} catch (err) {
+  console.error(`\n[setup] ${err.message}`);
+  process.exit(1);
+}
